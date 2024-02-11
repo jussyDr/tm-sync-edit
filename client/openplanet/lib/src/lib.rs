8b175f81322dd062e04a52fb9904de1f4b6d2b16
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-02-11 12:22:27 +0100

    Working hook in client

--- client/openplanet/lib/src/lib.rs
+++ client/openplanet/lib/src/lib.rs
@@ -1,79 +1,198 @@
-mod os;
-
 use std::{
-    ffi::{c_char, c_void},
+    ffi::c_void,
+    io::{Error, Result},
     iter,
-    panic::{self, PanicInfo},
+    mem::{size_of, MaybeUninit},
+    panic,
+    ptr::{self, null},
+    slice,
 };
 
-use os::{message_box, ModuleHandle, ProcessHandle};
 use windows_sys::Win32::{
-    Foundation::{BOOL, TRUE},
-    System::SystemServices::{
-        DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH,
+    Foundation::{BOOL, FALSE, TRUE},
+    System::{
+        Diagnostics::Debug::WriteProcessMemory,
+        LibraryLoader::GetModuleHandleW,
+        Memory::{VirtualAlloc, VirtualProtectEx, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE},
+        ProcessStatus::{GetModuleInformation, MODULEINFO},
+        SystemInformation::GetSystemInfo,
+        SystemServices::{
+            DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH,
+        },
+        Threading::{
+            GetCurrentProcessId, OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_OPERATION,
+            PROCESS_VM_READ, PROCESS_VM_WRITE,
+        },
     },
-    UI::WindowsAndMessaging::{MB_ICONERROR, MB_ICONINFORMATION},
+    UI::WindowsAndMessaging::{MessageBoxW, MB_ICONERROR, MB_ICONINFORMATION},
 };
 
-const PLACE_BLOCK_END_PATTERN: [u8; 28] = [
-    0x4c, 0x8d, 0x9c, 0x24, 0xc0, 0x00, 0x00, 0x00, 0x49, 0x8b, 0x5b, 0x38, 0x49, 0x8b, 0x73, 0x48,
-    0x49, 0x8b, 0xe3, 0x41, 0x5f, 0x41, 0x5e, 0x41, 0x5d, 0x5f, 0x5d, 0xc3,
-];
+#[allow(dead_code)]
+#[repr(u32)]
+enum DllCallReason {
+    ProcessAttach = DLL_PROCESS_ATTACH,
+    ProcessDetach = DLL_PROCESS_DETACH,
+    ThreadAttach = DLL_THREAD_ATTACH,
+    ThreadDetach = DLL_THREAD_DETACH,
+}
 
 #[no_mangle]
-extern "system" fn DllMain(
-    sync_edit_module: ModuleHandle,
-    call_reason: u32,
-    _reserved: *mut c_void,
-) -> BOOL {
+extern "system" fn DllMain(_module: isize, call_reason: DllCallReason, _reserved: isize) -> BOOL {
     match call_reason {
-        DLL_PROCESS_ATTACH => {
-            panic::set_hook(Box::new(move |panic_info| {
-                panic_hook(sync_edit_module, panic_info)
+        DllCallReason::ProcessAttach => {
+            panic::set_hook(Box::new(|panic_info| {
+                message_box(
+                    "SyncEdit.dll",
+                    &panic_info.to_string(),
+                    MessageBoxType::Error,
+                )
+                .unwrap();
             }));
         }
-        DLL_PROCESS_DETACH => {
+        DllCallReason::ProcessDetach => {
             let _ = panic::take_hook();
         }
-        DLL_THREAD_ATTACH => {}
-        DLL_THREAD_DETACH => {}
-        _ => unreachable!(),
+        _ => {}
     }
 
     TRUE
 }
 
 #[no_mangle]
-extern "system" fn Join(_host: *const c_char, _port: u16) {
-    let current_process = ProcessHandle::open_current().unwrap();
-    let trackmania_module = ModuleHandle::to_exe_file().unwrap();
-    let trackmania_module_memory = current_process.module_memory(trackmania_module).unwrap();
+extern "system" fn Init() {
+    let current_process_id = unsafe { GetCurrentProcessId() };
 
-    let place_block_end =
-        find_unique_pattern(trackmania_module_memory, &PLACE_BLOCK_END_PATTERN).unwrap();
+    let current_process = unsafe {
+        OpenProcess(
+            PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION,
+            FALSE,
+            current_process_id,
+        )
+    };
 
-    message_box("wat de fak", "huh!!!", MB_ICONINFORMATION).unwrap();
+    let exe_module = unsafe { GetModuleHandleW(null()) };
+
+    let mut exe_module_info = MaybeUninit::uninit();
+
+    unsafe {
+        GetModuleInformation(
+            current_process,
+            exe_module,
+            exe_module_info.as_mut_ptr(),
+            size_of::<MODULEINFO>() as u32,
+        )
+    };
+
+    let exe_module_info = unsafe { exe_module_info.assume_init() };
+
+    let exe_module_memory = unsafe {
+        slice::from_raw_parts(
+            exe_module_info.lpBaseOfDll as *const u8,
+            exe_module_info.SizeOfImage as usize,
+        )
+    };
+
+    let offset = find_pattern(
+        exe_module_memory,
+        &[
+            0x4c, 0x8d, 0x9c, 0x24, 0xc0, 0x00, 0x00, 0x00, 0x49, 0x8b, 0x5b, 0x38, 0x49, 0x8b,
+            0x73, 0x48, 0x49, 0x8b, 0xe3, 0x41, 0x5f, 0x41, 0x5e, 0x41, 0x5d, 0x5f, 0x5d, 0xc3,
+        ],
+    )
+    .unwrap();
+
+    let mut system_info = MaybeUninit::uninit();
+
+    unsafe { GetSystemInfo(system_info.as_mut_ptr()) };
+
+    let system_info = unsafe { system_info.assume_init() };
+
+    let executable_page = unsafe {
+        VirtualAlloc(
+            null(),
+            system_info.dwPageSize as usize,
+            MEM_COMMIT | MEM_RESERVE,
+            PAGE_EXECUTE_READWRITE,
+        )
+    };
+
+    let mut trampoline = [
+        0x49, 0x8b, 0xe3, 0x41, 0x5f, 0x41, 0x5e, 0x41, 0x5d, 0x5f, 0x5d, 0x50, 0x48, 0x89, 0xc1,
+        0x48, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xd0, 0x58, 0xc3,
+    ];
+
+    trampoline[17..25].copy_from_slice(&(callback as usize).to_le_bytes());
+
+    unsafe {
+        ptr::copy_nonoverlapping(
+            trampoline.as_ptr(),
+            executable_page as *mut u8,
+            trampoline.len(),
+        )
+    };
+
+    let ptr = unsafe { exe_module_memory.as_ptr().add(offset + 16) };
+
+    let mut old_protect = MaybeUninit::uninit();
+
+    unsafe {
+        VirtualProtectEx(
+            current_process,
+            ptr as *const c_void,
+            12,
+            PAGE_EXECUTE_READWRITE,
+            old_protect.as_mut_ptr(),
+        )
+    };
+
+    let mut hook = [0x48, 0xb9, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xe1];
+
+    hook[2..10].copy_from_slice(&(executable_page as usize).to_le_bytes());
+
+    let mut n_written = MaybeUninit::uninit();
+
+    unsafe {
+        WriteProcessMemory(
+            current_process,
+            ptr as *const c_void,
+            hook.as_ptr() as *const c_void,
+            hook.len(),
+            n_written.as_mut_ptr(),
+        );
+    }
+
+    message_box("SyncEdit.dll", "initialized", MessageBoxType::Info).unwrap();
 }
 
 #[no_mangle]
-extern "system" fn Update() {}
-
-fn panic_hook(sync_edit_module: ModuleHandle, panic_info: &PanicInfo) {
-    let dll_file_name = sync_edit_module.get_file_name().unwrap();
-
-    let text = panic_info.to_string();
-    let caption = dll_file_name.file_name().unwrap().to_string_lossy();
-
-    message_box(text, caption, MB_ICONERROR).unwrap();
+extern "system" fn Destroy() {
+    message_box("SyncEdit.dll", "destroyed", MessageBoxType::Info).unwrap();
 }
 
-fn encode_utf16_null_terminated(s: impl AsRef<str>) -> Vec<u16> {
-    s.as_ref().encode_utf16().chain(iter::once(0)).collect()
+extern "system" fn callback(_rax: u64) {
+    message_box("SyncEdit.dll", "callback", MessageBoxType::Info).unwrap();
 }
 
-fn find_unique_pattern(memory: &[u8], pattern: &[u8]) -> Option<usize> {
-    let mut pattern_offset = None;
+#[repr(u32)]
+enum MessageBoxType {
+    Error = MB_ICONERROR,
+    Info = MB_ICONINFORMATION,
+}
 
+fn message_box(caption: &str, text: &str, ty: MessageBoxType) -> Result<()> {
+    let caption: Vec<_> = caption.encode_utf16().chain(iter::once(0)).collect();
+    let text: Vec<_> = text.encode_utf16().chain(iter::once(0)).collect();
+
+    let result = unsafe { MessageBoxW(0, text.as_ptr(), caption.as_ptr(), ty as u32) };
+
+    if result == 0 {
+        return Err(Error::last_os_error());
+    }
+
+    Ok(())
+}
+
+fn find_pattern(memory: &[u8], pattern: &[u8]) -> Option<usize> {
     for offset in 0..memory.len() - pattern.len() {
         let mut matches = true;
 
@@ -85,17 +204,9 @@ fn find_unique_pattern(memory: &[u8], pattern: &[u8]) -> Option<usize> {
         }
 
         if matches {
-            if pattern_offset.is_some() {
-                return None;
-            }
-
-            pattern_offset = Some(offset);
+            return Some(offset);
         }
     }
 
-    pattern_offset
-}
-
-struct Hook<'a> {
-    trampoline: &'a [u8],
+    None
 }
--- client/openplanet/src/Main.as
+++ client/openplanet/src/Main.as
@@ -3,16 +3,13 @@ const string c_title = Icons::Syncthing + " Sync Edit";
 [Setting hidden]
 bool Setting_WindowEnabled = true;
 
-[Setting hidden]
-string Setting_Host = "0.0.0.0";
-
-[Setting hidden]
-string Setting_Port = "8369";
-
 Import::Library@ g_library = null;
-Import::Function@ g_libraryJoin = null;
 
-void Main() {}
+void Main() {
+    @g_library = Import::GetLibrary("SyncEdit.dll");
+    auto libraryInit = g_library.GetFunction("Init");
+    libraryInit.Call();
+}
 
 void RenderMenu() {
     if (UI::MenuItem(c_title, "", Setting_WindowEnabled)) {
@@ -29,25 +26,13 @@ void RenderInterface() {
         return;
     }
 
-    Setting_Host = UI::InputText("Host", Setting_Host, UI::InputTextFlags::CharsNoBlank);
-    Setting_Port = UI::InputText("Port", Setting_Port, UI::InputTextFlags::CharsDecimal);
-
-    if (UI::Button("Join")) {
-        @g_library = Import::GetLibrary("SyncEdit.dll");
-
-        if (g_library != null) {
-            @g_libraryJoin = g_library.GetFunction("Join");
-        }
-
-        if (g_libraryJoin != null) {
-            uint16 port = Text::ParseUInt(Setting_Port);
-            g_libraryJoin.Call(Setting_Host, port);
-        }
-    }
-
     UI::End();
 }
 
 void Update(float dt) {}
 
-void OnDestroyed() {}
+void OnDestroyed() {
+    auto libraryDestroy = g_library.GetFunction("Destroy");
+    libraryDestroy.Call();
+    @g_library = null;
+}
