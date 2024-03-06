7cbe7bf6fcafab0ab7b6659ef07813fd3c53fd0b
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-03-06 15:17:05 +0100

    Parse cli args in server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,11 @@
 use std::{
     error::Error,
     net::{IpAddr, Ipv4Addr, SocketAddr},
+    thread,
 };
 
 use bytes::Bytes;
+use clap::Parser;
 use futures_util::{SinkExt, TryStreamExt};
 use log::LevelFilter;
 use tm_sync_edit_shared::framed;
@@ -12,17 +14,41 @@ use tokio::{
     runtime, select, spawn,
 };
 
+#[derive(clap::Parser)]
+struct Args {
+    #[clap(short, long)]
+    port: Option<u16>,
+    #[clap(short, long)]
+    num_threads: Option<usize>,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::builder().filter_level(LevelFilter::Info).init();
 
-    let runtime = runtime::Builder::new_multi_thread().enable_io().build()?;
+    let args = Args::parse();
+
+    let num_threads = args
+        .num_threads
+        .unwrap_or(thread::available_parallelism()?.get());
+
+    log::info!("Number of threads: {num_threads}");
+
+    let mut runtime_builder = if num_threads == 1 {
+        runtime::Builder::new_current_thread()
+    } else {
+        runtime::Builder::new_multi_thread()
+    };
+
+    let runtime = runtime_builder.enable_io().build()?;
 
     runtime.block_on(async {
         let ip_addr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
-        let port = 8369;
+        let port = args.port.unwrap_or(8369);
         let socket_addr = SocketAddr::new(ip_addr, port);
 
         let tcp_listener = TcpListener::bind(socket_addr).await?;
+
+        log::info!("Listening on: {socket_addr}");
 
         loop {
             let (tcp_stream, socket_addr) = tcp_listener.accept().await?;
