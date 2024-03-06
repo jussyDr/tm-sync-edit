6827c936e83a01e87b118403b5c86de0c889b5ed
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-03-06 12:38:09 +0100

    More client experiments

--- client/openplanet/lib/src/lib.rs
+++ client/openplanet/lib/src/lib.rs
@@ -1,92 +1,62 @@
-use std::{
-    collections::HashMap,
-    error::Error,
-    ffi::{c_char, CStr, CString},
-    net::{IpAddr, SocketAddr},
-    ptr::null,
-    str::FromStr,
-    sync::Mutex,
-    thread,
+use std::{panic, sync::Mutex, thread};
+
+use native_dialog::{MessageDialog, MessageType};
+use tokio::{runtime, select, sync::oneshot};
+use windows_sys::Win32::{
+    Foundation::{BOOL, TRUE},
+    System::SystemServices::DLL_PROCESS_ATTACH,
 };
 
-use futures_util::TryStreamExt;
-use once_cell::sync::Lazy;
-use serde::Deserialize;
-use tokio::{net::TcpStream, runtime, select};
-use tokio_util::{codec::LengthDelimitedCodec, sync::CancellationToken};
+static SHOULD_JOIN: Mutex<bool> = Mutex::new(false);
 
-static STATE: Mutex<State> = Mutex::new(State::Disconnected);
-
-static STATUS_TEXT: Mutex<Option<CString>> = Mutex::new(None);
-
-static EDITOR: Mutex<usize> = Mutex::new(0);
-
-static BLOCK_INFOS: Lazy<Mutex<HashMap<String, usize>>> = Lazy::new(|| Mutex::new(HashMap::new()));
-
-static ITEM_MODELS: Lazy<Mutex<HashMap<String, usize>>> = Lazy::new(|| Mutex::new(HashMap::new()));
-
-static SHOULD_OPEN_EDITOR: Mutex<bool> = Mutex::new(false);
+static JOIN_SUCCESS_SENDER: Mutex<Option<oneshot::Sender<bool>>> = Mutex::new(None);
 
 #[no_mangle]
-extern "C" fn ButtonLabel() -> *const c_char {
-    match *STATE.lock().unwrap() {
-        State::Disconnected => "Join\0".as_ptr() as *const c_char,
-        State::Joining { .. } => "Cancel\0".as_ptr() as *const c_char,
+extern "system" fn DllMain(_dll_module: usize, call_reason: u32, _reserved: usize) -> BOOL {
+    if call_reason == DLL_PROCESS_ATTACH {
+        panic::set_hook(Box::new(|panic_info| {
+            MessageDialog::new()
+                .set_type(MessageType::Error)
+                .set_title("SyncEdit.dll")
+                .set_text(&panic_info.to_string())
+                .show_alert()
+                .unwrap();
+        }));
     }
+
+    TRUE
 }
 
 #[no_mangle]
-extern "C" fn ButtonPressed(host: *const c_char, port: *const c_char) {
-    let mut state = STATE.lock().unwrap();
+extern "system" fn Join() {
+    thread::spawn(|| {
+        let runtime = runtime::Builder::new_current_thread().build().unwrap();
 
-    match *state {
-        State::Disconnected => {
-            let cancellation_token = CancellationToken::new();
-            let child_token = cancellation_token.child_token();
+        runtime.block_on(async {
+            let (join_success_sender, join_success_receiver) = oneshot::channel();
 
-            *state = State::Joining { cancellation_token };
-            *STATUS_TEXT.lock().unwrap() = Some(CString::new("connecting...").unwrap());
+            *JOIN_SUCCESS_SENDER.lock().unwrap() = Some(join_success_sender);
 
-            let host = unsafe { CStr::from_ptr(host).to_str().unwrap().to_owned() };
-            let port = unsafe { u16::from_str(CStr::from_ptr(port).to_str().unwrap()).unwrap() };
+            *SHOULD_JOIN.lock().unwrap() = true;
 
-            thread::spawn(move || {
-                if let Err(error) = connection(child_token, host.as_str(), port) {
-                    *STATE.lock().unwrap() = State::Disconnected;
-                    *STATUS_TEXT.lock().unwrap() = Some(CString::new(error.to_string()).unwrap());
-                }
-            });
-        }
-        State::Joining {
-            ref cancellation_token,
-        } => {
-            cancellation_token.cancel();
+            let join_success = join_success_receiver.await.unwrap();
 
-            *state = State::Disconnected;
-            *STATUS_TEXT.lock().unwrap() = Some(CString::new("canceled").unwrap());
-        }
-    }
+            MessageDialog::new()
+                .set_type(MessageType::Info)
+                .set_title("SyncEdit.dll")
+                .set_text(&join_success.to_string())
+                .show_alert()
+                .unwrap();
+        });
+    });
 }
 
 #[no_mangle]
-extern "C" fn StatusText() -> *const c_char {
-    match &*STATUS_TEXT.lock().unwrap() {
-        None => null(),
-        Some(status_text) => status_text.as_ptr(),
-    }
-}
+extern "system" fn ShouldJoin() -> bool {
+    let mut should_join = SHOULD_JOIN.lock().unwrap();
 
-#[no_mangle]
-extern "C" fn SetEditor(editor: usize) {
-    *EDITOR.lock().unwrap() = editor;
-}
-
-#[no_mangle]
-extern "C" fn ShouldOpenEditor() -> bool {
-    let mut should_open_editor = SHOULD_OPEN_EDITOR.lock().unwrap();
-
-    if *should_open_editor {
-        *should_open_editor = false;
+    if *should_join {
+        *should_join = false;
 
         true
     } else {
@@ -95,133 +65,12 @@ extern "C" fn ShouldOpenEditor() -> bool {
 }
 
 #[no_mangle]
-extern "C" fn RegisterBlockInfo(id: *const c_char, block_info: usize) {
-    let id = unsafe { CStr::from_ptr(id).to_str().unwrap().to_owned() };
-
-    BLOCK_INFOS.lock().unwrap().insert(id, block_info);
-}
-
-#[no_mangle]
-extern "C" fn RegisterItemModel(id: *const c_char, item_model: usize) {
-    let id = unsafe { CStr::from_ptr(id).to_str().unwrap().to_owned() };
-
-    ITEM_MODELS.lock().unwrap().insert(id, item_model);
-}
-
-fn connection(
-    cancellation_token: CancellationToken,
-    host: &str,
-    port: u16,
-) -> Result<(), Box<dyn Error>> {
-    let runtime = runtime::Builder::new_current_thread().enable_io().build()?;
-
-    runtime.block_on(async {
-        let ip_addr = IpAddr::from_str(host)?;
-        let socket_addr = SocketAddr::new(ip_addr, port);
-
-        let tcp_stream = select! {
-            _ = cancellation_token.cancelled() => return Ok(()),
-            tcp_stream = TcpStream::connect(socket_addr) => tcp_stream?,
-        };
-
-        *STATUS_TEXT.lock().unwrap() = Some(CString::new("connected").unwrap());
-
-        let mut framed_tcp_stream = LengthDelimitedCodec::builder().new_framed(tcp_stream);
-
-        let game_fns = GameFns::find()?;
-
-        let frame = select! {
-            _ = cancellation_token.cancelled() => return Ok(()),
-            frame = framed_tcp_stream.try_next() => frame?.ok_or("connection lost")?.freeze(),
-        };
-
-        let map: Map = postcard::from_bytes(&frame)?;
-
-        for block in map.blocks {
-            let block_info = *BLOCK_INFOS.lock().unwrap().get(&block.id).ok_or("")?;
-        }
-
-        for free_block in map.free_blocks {
-            let block_info = *BLOCK_INFOS.lock().unwrap().get(&free_block.id).ok_or("")?;
-        }
-
-        for item in map.items {
-            let item_model = *ITEM_MODELS.lock().unwrap().get(&item.id).ok_or("")?;
-        }
-
-        Ok(())
-    })
-}
-
-enum State {
-    Disconnected,
-    Joining {
-        cancellation_token: CancellationToken,
-    },
-}
-
-struct GameFns;
-
-impl GameFns {
-    fn find() -> Result<Self, Box<dyn Error>> {
-        Ok(Self)
-    }
-
-    fn place_block(
-        &self,
-        editor: usize,
-        block_info: usize,
-        dir: u32,
-        x: u32,
-        y: u32,
-        z: u32,
-        elem_color: u8,
-    ) {
-        todo!()
-    }
-}
-
-#[derive(Deserialize)]
-struct Map {
-    blocks: Vec<Block>,
-    free_blocks: Vec<FreeBlock>,
-    items: Vec<Item>,
-}
-
-#[derive(Deserialize)]
-struct Block {
-    id: String,
-    dir: Direction,
-    x: u8,
-    y: u8,
-    z: u8,
-    elem_color: ElemColor,
-}
-
-#[derive(Deserialize)]
-struct FreeBlock {
-    id: String,
-}
-
-#[derive(Deserialize)]
-struct Item {
-    id: String,
-}
-
-#[derive(Deserialize)]
-enum Direction {
-    North,
-    East,
-    South,
-    West,
-}
-
-#[derive(Deserialize)]
-enum ElemColor {
-    Default,
-    White,
-    Green,
-    Blue,
-    Red,
-    Black,
+extern "system" fn JoinSuccess(success: bool) {
+    JOIN_SUCCESS_SENDER
+        .lock()
+        .unwrap()
+        .take()
+        .unwrap()
+        .send(success)
+        .unwrap();
 }
--- client/openplanet/src/Main.as
+++ client/openplanet/src/Main.as
@@ -1,212 +1,99 @@
-const string c_title = "Sync Edit";
-const string c_libraryPath = "SyncEdit.dll";
-
-[Setting hidden]
-string Setting_Host = "127.0.0.1";
-
-[Setting hidden]
-string Setting_Port = "8369";
-
 Library@ g_library = null;
 
 void Main() {
-    auto library = Import::GetLibrary(c_libraryPath);
-
-    if (library is null) {
-        return;
-    }
-
-    auto buttonLabel = library.GetFunction("ButtonLabel");
-
-    if (buttonLabel is null) {
-        return;
-    }
-
-    auto buttonPressed = library.GetFunction("ButtonPressed");
-
-    if (buttonPressed is null) {
-        return;
-    }
-
-    auto statusText = library.GetFunction("StatusText");
-
-    if (statusText is null) {
-        return;
-    }
-
-    auto setEditor = library.GetFunction("SetEditor");
-
-    if (setEditor is null) {
-        return;
-    }
-
-    auto registerBlockInfo = library.GetFunction("RegisterBlockInfo");
-
-    if (registerBlockInfo is null) {
-        return;
-    }
-
-    auto registerItemModel = library.GetFunction("RegisterItemModel");
-
-    if (registerItemModel is null) {
-        return;
-    }
-
-    @g_library = Library(library, buttonLabel, buttonPressed, statusText, setEditor, registerBlockInfo, registerItemModel);
-
-    LoadBlockInfos();
-    LoadItemModels();
+    LoadLibrary();
 }
 
 void RenderInterface() {
-    if (UI::Begin(c_title)) {
-        if (g_library is null) {
-            UI::Text("failed to load library: " + c_libraryPath);
-        } else {
-            Setting_Host = UI::InputText("Host", Setting_Host, UI::InputTextFlags::CharsNoBlank);
-
-            Setting_Port = UI::InputText("Port", Setting_Port, UI::InputTextFlags::CharsNoBlank);
-
-            if (UI::Button(g_library.ButtonLabel())) {
-                g_library.ButtonPressed(Setting_Host, Setting_Port);
-            }
-
-            UI::Text(g_library.StatusText());
+    if (UI::Begin("Sync Edit")) {
+        if (UI::Button("Join")) {
+            Join();
         }
 
         UI::End();
     }
 }
 
+
 void Update(float dt) {
-    auto maniaPlanet = cast<CGameManiaPlanet>(GetApp());
-    auto switcher = maniaPlanet.Switcher;
+    if (g_library !is null) {
+        if (g_library.ShouldJoin()) {
+            print("joining");
 
-    if (switcher.ModuleStack.Length >= 1) {
-        auto currentSwitcherModule = switcher.ModuleStack[switcher.ModuleStack.Length - 1];
-
-        if (cast<CTrackManiaMenus>(currentSwitcherModule) !is null) {
-            maniaPlanet.ManiaTitleControlScriptAPI.EditNewMap2("Stadium", "48x48Screen155Day", "", "CarSport", "", false, "", "");
-        } else if (cast<CGameEditorMediaTracker>(currentSwitcherModule) !is null) {
-            auto mediaTrackerEditor = cast<CGameEditorMediaTracker>(currentSwitcherModule);
-            auto mediaTrackerEditorPluginAPI = cast<CGameEditorMediaTrackerPluginAPI>(mediaTrackerEditor.PluginAPI);
-            mediaTrackerEditorPluginAPI.Quit();
-        } else if (cast<CGameCtnEditorFree>(currentSwitcherModule) is null) {
-            maniaPlanet.BackToMainMenu();
+            g_library.JoinSuccess(true);
         }
     }
 }
 
 void OnDestroyed() {
+    FreeLibrary();
+}
+
+void Join() {
+    if (g_library !is null) {
+        g_library.Join();
+    }
+}
+
+void LoadLibrary() {
+    auto library = Import::GetLibrary("SyncEdit.dll");
+
+    if (library is null) {
+        return;
+    }
+
+    auto join = library.GetFunction("Join");
+
+    if (join is null) {
+        return;
+    }
+
+    auto shouldJoin = library.GetFunction("ShouldJoin");
+
+    if (shouldJoin is null) {
+        return;
+    }
+
+    auto joinSuccess = library.GetFunction("JoinSuccess");
+
+    if (joinSuccess is null) {
+        return;
+    }
+
+    @g_library = Library(library, join, shouldJoin, joinSuccess);
+}
+
+void FreeLibrary() {
     @g_library = null;
 }
 
-void LoadBlockInfos() {
-    if (g_library !is null) {
-        auto folder = Fids::GetGameFolder("GameData\\Stadium\\GameCtnBlockInfo");
-        LoadBlockInfosInner(folder);
-    }
-}
-
-void LoadBlockInfosInner(CSystemFidsFolder@ folder) {
-    if (folder is null) {
-        return;
-    }
-
-    for (uint32 i = 0; i < folder.Leaves.Length; i++) {
-        auto fid = folder.Leaves[i];
-        auto blockInfo = cast<CGameCtnBlockInfo>(Fids::Preload(fid));
-        g_library.RegisterBlockInfo(blockInfo.IdName, blockInfo);
-
-        if (i % 500 == 0) {
-            yield();
-        }
-    }
-
-    for (uint32 i = 0; i < folder.Trees.Length; i++) {
-        LoadBlockInfosInner(folder.Trees[i]);
-    }
-}
-
-void LoadItemModels() {
-    if (g_library !is null) {
-        auto folder = Fids::GetGameFolder("GameData\\Stadium\\Items");
-        LoadItemModelsInner(folder);
-    }
-}
-
-void LoadItemModelsInner(CSystemFidsFolder@ folder) {
-    if (folder is null) {
-        return;
-    }
-
-    for (uint32 i = 0; i < folder.Leaves.Length; i++) {
-        auto fid = folder.Leaves[i];
-        auto itemModel = cast<CGameItemModel>(Fids::Preload(fid));
-        g_library.RegisterItemModel(itemModel.IdName, itemModel);
-
-        if (i % 500 == 0) {
-            yield();
-        }
-    }
-
-    for (uint32 i = 0; i < folder.Trees.Length; i++) {
-        LoadItemModelsInner(folder.Trees[i]);
-    }
-}
-
 class Library {
-    private Import::Library@ library;
-    private Import::Function@ buttonLabel;
-    private Import::Function@ buttonPressed;
-    private Import::Function@ statusText;
-    private Import::Function@ setEditor;
-    private Import::Function@ registerBlockInfo;
-    private Import::Function@ registerItemModel;
-
+    private Import::Library@ m_library;
+    private Import::Function@ m_join;
+    private Import::Function@ m_shouldJoin;
+    private Import::Function@ m_joinSuccess;
+    
     Library(
-        Import::Library@ library, 
-        Import::Function@ buttonLabel, 
-        Import::Function@ buttonPressed,
-        Import::Function@ statusText,
-        Import::Function@ setEditor,
-        Import::Function@ registerBlockInfo,
-        Import::Function@ registerItemModel
+        Import::Library@ library,
+        Import::Function@ join,
+        Import::Function@ shouldJoin,
+        Import::Function@ joinSuccess
     ) {
-        @this.library = library;
-        @this.buttonLabel = buttonLabel;
-        @this.buttonPressed = buttonPressed;
-        @this.statusText = statusText;
-        @this.setEditor = setEditor;
-        @this.registerBlockInfo = registerBlockInfo;
-        @this.registerItemModel = registerItemModel;
+        @m_library = library;
+        @m_join = join;
+        @m_shouldJoin = shouldJoin;
+        @m_joinSuccess = joinSuccess;
     }
 
-    string ButtonLabel() {
-        return buttonLabel.CallString();
+    void Join() {
+        m_join.Call();
     }
 
-    void ButtonPressed(const string&in host, const string&in port) {
-        buttonPressed.Call(host, port);
+    bool ShouldJoin() {
+        return m_shouldJoin.CallBool();
     }
 
-    string StatusText() {
-        return statusText.CallString();
-    }
-
-    void SetEditor(const CGameCtnEditor@ editor) {
-        if (editor is null) {
-            setEditor.Call(uint64(0));
-        } else {
-            setEditor.Call(editor);
-        }
-    }
-
-    void RegisterBlockInfo(const string&in id, const CGameCtnBlockInfo@ blockInfo) {
-        registerBlockInfo.Call(id, blockInfo);
-    }
-
-    void RegisterItemModel(const string&in id, const CGameItemModel@ itemModel) {
-        registerItemModel.Call(id, itemModel);
+    void JoinSuccess(bool success) {
+        m_joinSuccess.Call(success);
     }
 }
