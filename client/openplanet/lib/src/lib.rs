74cb4153d2214cbb5abd17103b6b447ab345f70d
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-03-09 09:30:52 +0100

    Add function patterns

--- client/openplanet/lib/src/lib.rs
+++ client/openplanet/lib/src/lib.rs
@@ -2,16 +2,20 @@ use std::{
     collections::HashMap,
     error::Error,
     ffi::{c_char, CStr, CString},
+    io,
+    mem::{size_of, transmute, MaybeUninit},
     net::{IpAddr, SocketAddr},
     panic,
     pin::pin,
     ptr::null,
+    slice,
     str::FromStr,
     sync::{Arc, Mutex},
     thread,
 };
 
 use futures_util::{StreamExt, TryStreamExt};
+use memchr::memmem;
 use native_dialog::{MessageDialog, MessageType};
 use tm_sync_edit_shared::{framed_tcp_stream, Map};
 use tokio::{
@@ -21,7 +25,12 @@ use tokio::{
 };
 use windows_sys::Win32::{
     Foundation::{BOOL, TRUE},
-    System::SystemServices::DLL_PROCESS_ATTACH,
+    System::{
+        LibraryLoader::GetModuleHandleW,
+        ProcessStatus::{GetModuleInformation, MODULEINFO},
+        SystemServices::DLL_PROCESS_ATTACH,
+        Threading::GetCurrentProcess,
+    },
 };
 
 static BLOCK_INFOS: Mutex<Option<HashMap<String, usize>>> = Mutex::new(None);
@@ -172,6 +181,8 @@ async fn join_inner_inner(socket_addr: SocketAddr) -> Result<(), Box<dyn Error>>
         result = join_inner_inner_inner() => result?
     }
 
+    let game_fns = GameFns::find()?;
+
     loop {
         select! {
             result = framed_tcp_stream.try_next() => match result? {
@@ -196,4 +207,116 @@ async fn join_inner_inner_inner() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+struct GameFns {
+    place_block_fn: unsafe extern "system" fn(),
+    remove_block_fn: unsafe extern "system" fn(),
+    place_item_fn: unsafe extern "system" fn(),
+    remove_item_fn: unsafe extern "system" fn(),
+}
+
+impl GameFns {
+    fn find() -> Result<Self, Box<dyn Error>> {
+        let process = unsafe { GetCurrentProcess() };
+        let module = unsafe { GetModuleHandleW(null()) };
+
+        let mut module_info = MaybeUninit::uninit();
+
+        let success = unsafe {
+            GetModuleInformation(
+                process,
+                module,
+                module_info.as_mut_ptr(),
+                size_of::<MODULEINFO>() as u32,
+            )
+        };
+
+        if success == 0 {
+            return Err(io::Error::last_os_error().into());
+        }
+
+        let module_info = unsafe { module_info.assume_init() };
+
+        let module_memory = unsafe {
+            slice::from_raw_parts(
+                module_info.lpBaseOfDll as *const u8,
+                module_info.SizeOfImage as usize,
+            )
+        };
+
+        let place_block_fn_offset = memmem::find(
+            module_memory,
+            &[
+                0x48, 0x89, 0x5c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x20, 0x4c, 0x89, 0x44, 0x24,
+                0x18, 0x55,
+            ],
+        )
+        .ok_or("")?;
+
+        let remove_block_fn_offset = memmem::find(
+            module_memory,
+            &[
+                0x48, 0x89, 0x5c, 0x24, 0x08, 0x48, 0x89, 0x6c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24,
+                0x18, 0x57, 0x48, 0x83, 0xec, 0x40, 0x83, 0x7c, 0x24, 0x70, 0x00,
+            ],
+        )
+        .ok_or("")?;
+
+        let place_item_fn_offset = memmem::find(
+            module_memory,
+            &[
+                0x48, 0x89, 0x5c, 0x24, 0x10, 0x48, 0x89, 0x6c, 0x24, 0x18, 0x48, 0x89, 0x74, 0x24,
+                0x20, 0x57, 0x48, 0x83, 0xec, 0x40, 0x49, 0x8b, 0xf9,
+            ],
+        )
+        .ok_or("")?;
+
+        let remove_item_fn_offset = memmem::find(
+            module_memory,
+            &[
+                0x48, 0x89, 0x5c, 0x24, 0x08, 0x57, 0x48, 0x83, 0xec, 0x30, 0x48, 0x8b, 0xfa, 0x48,
+                0x8b, 0xd9, 0x48, 0x85, 0xd2, 0x0f, 0x84, 0xe6, 0x00, 0x00, 0x00,
+            ],
+        )
+        .ok_or("")?;
+
+        let place_block_fn =
+            unsafe { transmute(module_memory.as_ptr().add(place_block_fn_offset)) };
+
+        let remove_block_fn =
+            unsafe { transmute(module_memory.as_ptr().add(remove_block_fn_offset)) };
+
+        let place_item_fn = unsafe { transmute(module_memory.as_ptr().add(place_item_fn_offset)) };
+
+        let remove_item_fn =
+            unsafe { transmute(module_memory.as_ptr().add(remove_item_fn_offset)) };
+
+        Ok(Self {
+            place_block_fn,
+            remove_block_fn,
+            place_item_fn,
+            remove_item_fn,
+        })
+    }
+
+    fn place_block(&self) {
+        unsafe { (self.place_block_fn)() }
+    }
+
+    fn place_free_block(&self) {
+        unsafe { (self.place_block_fn)() }
+    }
+
+    fn remove_block(&self) {
+        unsafe { (self.remove_block_fn)() }
+    }
+
+    fn place_item(&self) {
+        unsafe { (self.place_item_fn)() }
+    }
+
+    fn remove_item(&self) {
+        unsafe { (self.remove_item_fn)() }
+    }
 }
