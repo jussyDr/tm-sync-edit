fc9a10459d33f8f69a4a844bf2e2b9016f71763f
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-03-24 11:19:40 +0100

    more stuff

--- client/openplanet/lib/src/game.rs
+++ client/openplanet/lib/src/game.rs
@@ -14,9 +14,7 @@ use windows_sys::Win32::{
     System::{
         Diagnostics::Debug::WriteProcessMemory,
         LibraryLoader::GetModuleHandleW,
-        Memory::{
-            VirtualAlloc, VirtualFree, MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_EXECUTE_READWRITE,
-        },
+        Memory::{VirtualAlloc, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE},
         ProcessStatus::{GetModuleInformation, MODULEINFO},
         Threading::{
             GetCurrentProcess, GetCurrentProcessId, OpenProcess, PROCESS_QUERY_INFORMATION,
@@ -217,24 +215,6 @@ pub fn hook_place_block() -> Result<PlaceBlockHook, Box<dyn Error>> {
 
     hook_code[2..2 + 8].copy_from_slice(&(trampoline_ptr as usize).to_le_bytes());
 
-    let mut n_written = MaybeUninit::uninit();
-
-    let result = unsafe {
-        WriteProcessMemory(
-            current_process,
-            exe_module_memory
-                .as_ptr()
-                .add(place_block_fn_end_offset + 16) as *const c_void,
-            hook_code.as_ptr() as *const c_void,
-            12,
-            n_written.as_mut_ptr(),
-        )
-    };
-
-    if result == 0 {
-        return Err(io::Error::last_os_error().into());
-    }
-
     let hook_ptr = unsafe {
         exe_module_memory
             .as_ptr()
@@ -246,20 +226,11 @@ pub fn hook_place_block() -> Result<PlaceBlockHook, Box<dyn Error>> {
             current_process,
             hook_ptr,
             hook_code.as_ptr() as *const c_void,
-            12,
+            hook_code.len(),
         )?
     };
 
-    unsafe { VirtualFree(trampoline_ptr, trampoline_code.len(), MEM_RELEASE) };
-
     unsafe { CloseHandle(current_process) };
-
-    MessageDialog::new()
-        .set_type(MessageType::Info)
-        .set_title("SyncEdit.dll")
-        .set_text("placed block!")
-        .show_confirm()
-        .unwrap();
 
     Ok(PlaceBlockHook { hook_ptr })
 }
@@ -285,6 +256,8 @@ impl Drop for PlaceBlockHook {
             )
             .unwrap()
         };
+
+        // unsafe { VirtualFree(trampoline_ptr, trampoline_code.len(), MEM_RELEASE) };
     }
 }
 
@@ -297,8 +270,149 @@ unsafe extern "system" fn place_block_callback(block: *mut u8) {
         .unwrap();
 }
 
-pub fn hook_remove_block() -> Result<(), Box<dyn Error>> {
-    Ok(())
+pub fn hook_remove_block() -> Result<RemoveBlockHook, Box<dyn Error>> {
+    let current_process = unsafe { open_current_process()? };
+
+    let exe_module = unsafe { GetModuleHandleW(null()) };
+
+    let mut exe_module_info = MaybeUninit::uninit();
+
+    let success = unsafe {
+        GetModuleInformation(
+            current_process,
+            exe_module,
+            exe_module_info.as_mut_ptr(),
+            size_of::<MODULEINFO>() as u32,
+        )
+    };
+
+    if success == 0 {
+        return Err(io::Error::last_os_error().into());
+    }
+
+    let exe_module_info = unsafe { exe_module_info.assume_init() };
+
+    let exe_module_memory = unsafe {
+        slice::from_raw_parts(
+            exe_module_info.lpBaseOfDll as *const u8,
+            exe_module_info.SizeOfImage as usize,
+        )
+    };
+
+    let remove_block_fn_offset = memmem::find(
+        exe_module_memory,
+        &[
+            0x48, 0x89, 0x5c, 0x24, 0x08, 0x48, 0x89, 0x6c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24,
+            0x18, 0x57, 0x48, 0x83, 0xec, 0x40, 0x83, 0x7c, 0x24, 0x70, 0x00,
+        ],
+    )
+    .ok_or("failed to find remove block function")?;
+
+    let hook_ptr = unsafe { exe_module_memory.as_ptr().add(remove_block_fn_offset) };
+    let hook_end_ptr = unsafe { hook_ptr.add(15) };
+
+    let mut trampoline_code = [
+        0x48, 0x89, 0x5c, 0x24, 0x08, // mov [rsp + 8], rbx
+        0x48, 0x89, 0x6c, 0x24, 0x10, // mov [rsp + 16], rbp
+        0x48, 0x89, 0x74, 0x24, 0x18, // mov [rsp + 24], rsi
+        0x51, // push rcx
+        0x52, // push rdx
+        0x41, 0x50, // push r8
+        0x41, 0x51, // push r9
+        0x48, 0x83, 0xec, 0x20, // sub rsp, 32
+        0x48, 0x89, 0xd1, // mov rcx, rdx
+        0x48, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, // mov rax, ????????
+        0xff, 0xd0, // call rax
+        0x48, 0x83, 0xc4, 0x20, // add rsp, 32
+        0x41, 0x59, // pop r9
+        0x41, 0x58, // pop r8
+        0x5a, // pop rdx
+        0x59, // pop rcx
+        0x48, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, // mov rax, ????????
+        0xff, 0xe0, // jmp rax
+    ];
+
+    trampoline_code[30..30 + 8].copy_from_slice(&(remove_block_callback as usize).to_le_bytes());
+    trampoline_code[52..52 + 8].copy_from_slice(&(hook_end_ptr as usize).to_le_bytes());
+
+    let trampoline_ptr = unsafe {
+        VirtualAlloc(
+            null(),
+            trampoline_code.len(),
+            MEM_COMMIT | MEM_RESERVE,
+            PAGE_EXECUTE_READWRITE,
+        )
+    };
+
+    if trampoline_ptr.is_null() {
+        return Err(io::Error::last_os_error().into());
+    }
+
+    let trampoline =
+        unsafe { slice::from_raw_parts_mut(trampoline_ptr as *mut u8, trampoline_code.len()) };
+
+    trampoline.copy_from_slice(&trampoline_code);
+
+    let mut hook_code: [u8; 15] = [
+        0x48, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, // mov rax, ????????
+        0xff, 0xe0, // jmp rax
+        0x90, // nop
+        0x90, // nop
+        0x90, // nop
+    ];
+
+    hook_code[2..2 + 8].copy_from_slice(&(trampoline_ptr as usize).to_le_bytes());
+
+    unsafe {
+        write_process_memory(
+            current_process,
+            hook_ptr as *const c_void,
+            hook_code.as_ptr() as *const c_void,
+            hook_code.len(),
+        )?
+    };
+
+    unsafe { CloseHandle(current_process) };
+
+    Ok(RemoveBlockHook {
+        hook_ptr: hook_ptr as *const c_void,
+    })
+}
+
+pub struct RemoveBlockHook {
+    hook_ptr: *const c_void,
+}
+
+impl Drop for RemoveBlockHook {
+    fn drop(&mut self) {
+        let current_process = unsafe { open_current_process().unwrap() };
+
+        let original_code: [u8; 15] = [
+            0x48, 0x89, 0x5c, 0x24, 0x08, 0x48, 0x89, 0x6c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24,
+            0x18,
+        ];
+
+        unsafe {
+            write_process_memory(
+                current_process,
+                self.hook_ptr,
+                original_code.as_ptr() as *const c_void,
+                original_code.len(),
+            )
+            .unwrap()
+        };
+
+        // unsafe { VirtualFree(trampoline_ptr, trampoline_code.len(), MEM_RELEASE) };
+    }
+}
+
+unsafe extern "system" fn remove_block_callback(block: *mut u8) {
+    MessageDialog::new()
+        .set_type(MessageType::Info)
+        .set_title("SyncEdit.dll")
+        .set_text("removed block!")
+        .show_confirm()
+        .unwrap();
 }
 
 pub fn hook_place_item() -> Result<(), Box<dyn Error>> {
