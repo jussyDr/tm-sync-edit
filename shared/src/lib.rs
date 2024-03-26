bbeffa333b177a92c376d29d7fada7c62aea0f78
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-03-26 19:13:19 +0100

    wat doink

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,1 +1,6 @@
+use tokio::net::TcpStream;
+use tokio_util::codec::{Decoder, Framed, LengthDelimitedCodec};
 
+pub fn framed_tcp_stream(tcp_stream: TcpStream) -> Framed<TcpStream, LengthDelimitedCodec> {
+    LengthDelimitedCodec::new().framed(tcp_stream)
+}
