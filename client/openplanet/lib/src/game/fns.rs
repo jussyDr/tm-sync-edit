d9fc7178d92be972c5233a1f68833e0428021fa6
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-05-09 09:16:57 +0200

    Generalize fn finding

--- client/openplanet/lib/src/game/fns.rs
+++ client/openplanet/lib/src/game/fns.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error,
     ffi::{c_char, CStr},
-    mem::{transmute, MaybeUninit},
+    mem::{transmute_copy, MaybeUninit},
     ptr::{null, null_mut},
 };
 
@@ -14,6 +14,20 @@ use ordered_float::NotNan;
 
 use super::{Block, BlockInfo, FidFile, Item, ItemModel, ItemParams, MapEditor, Nod};
 
+fn find_fn<T>(
+    exe_module_memory: &[u8],
+    pattern: &[u8],
+    pattern_offset: isize,
+) -> Result<T, Box<dyn Error>> {
+    let offset = memmem::find(exe_module_memory, pattern)
+        .ok_or("failed to find function pattern")? as isize
+        + pattern_offset;
+
+    let f = unsafe { transmute_copy::<*const u8, T>(&exe_module_memory.as_ptr().offset(offset)) };
+
+    Ok(f)
+}
+
 type PreloadFidFnType =
     unsafe extern "system" fn(ret_nod: *mut *mut Nod, fid: *mut FidFile, nod: *mut u8);
 
@@ -22,20 +36,11 @@ pub struct PreloadFidFn(PreloadFidFnType);
 
 impl PreloadFidFn {
     pub fn find(exe_module_memory: &[u8]) -> Result<Self, Box<dyn Error>> {
-        let preload_fid_fn_offset = memmem::find(
-            exe_module_memory,
-            &[
-                0x48, 0x33, 0xc4, 0x48, 0x89, 0x84, 0x24, 0xa0, 0x00, 0x00, 0x00, 0x49, 0x8b, 0xf8,
-            ],
-        )
-        .ok_or("failed to find PreloadFid function pattern")?
-            - 18;
+        let pattern = &[
+            0x48, 0x33, 0xc4, 0x48, 0x89, 0x84, 0x24, 0xa0, 0x00, 0x00, 0x00, 0x49, 0x8b, 0xf8,
+        ];
 
-        let preload_fid_fn = unsafe {
-            transmute::<*const u8, PreloadFidFnType>(
-                exe_module_memory.as_ptr().add(preload_fid_fn_offset),
-            )
-        };
+        let preload_fid_fn = find_fn(exe_module_memory, pattern, -18)?;
 
         Ok(Self(preload_fid_fn))
     }
@@ -63,12 +68,9 @@ pub struct IdNameFn(IdNameFnType);
 
 impl IdNameFn {
     pub fn find(exe_module_memory: &[u8]) -> Result<Self, Box<dyn Error>> {
-        let id_name_fn_offset = memmem::find(exe_module_memory, &[0x8b, 0x11, 0x8b, 0xc2, 0x25])
-            .ok_or("failed to find IdName function pattern")?;
+        let pattern = &[0x8b, 0x11, 0x8b, 0xc2, 0x25];
 
-        let id_name_fn = unsafe {
-            transmute::<*const u8, IdNameFnType>(exe_module_memory.as_ptr().add(id_name_fn_offset))
-        };
+        let id_name_fn = find_fn(exe_module_memory, pattern, 0)?;
 
         Ok(Self(id_name_fn))
     }
@@ -87,20 +89,12 @@ pub struct PreloadBlockInfoFn(PreloadBlockInfoFnType);
 
 impl PreloadBlockInfoFn {
     pub fn find(exe_module_memory: &[u8]) -> Result<Self, Box<dyn Error>> {
-        let preload_block_info_fn_offset = memmem::find(
-            exe_module_memory,
-            &[
-                0x40, 0x55, 0x56, 0x41, 0x54, 0x41, 0x56, 0x48, 0x8d, 0xac, 0x24, 0x98, 0xfa, 0xff,
-                0xff,
-            ],
-        )
-        .ok_or("failed to find PreloadBlockInfo function pattern")?;
+        let pattern = &[
+            0x40, 0x55, 0x56, 0x41, 0x54, 0x41, 0x56, 0x48, 0x8d, 0xac, 0x24, 0x98, 0xfa, 0xff,
+            0xff,
+        ];
 
-        let preload_block_info_fn = unsafe {
-            transmute::<*const u8, PreloadBlockInfoFnType>(
-                exe_module_memory.as_ptr().add(preload_block_info_fn_offset),
-            )
-        };
+        let preload_block_info_fn = find_fn(exe_module_memory, pattern, 0)?;
 
         Ok(Self(preload_block_info_fn))
     }
@@ -137,20 +131,12 @@ pub struct PlaceBlockFn(PlaceBlockFnType);
 
 impl PlaceBlockFn {
     pub fn find(exe_module_memory: &[u8]) -> Result<Self, Box<dyn Error>> {
-        let place_block_fn_offset = memmem::find(
-            exe_module_memory,
-            &[
-                0x48, 0x89, 0x5c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x20, 0x4c, 0x89, 0x44, 0x24,
-                0x18, 0x55,
-            ],
-        )
-        .ok_or("failed to find PlaceBlock function pattern")?;
+        let pattern = &[
+            0x48, 0x89, 0x5c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24, 0x20, 0x4c, 0x89, 0x44, 0x24,
+            0x18, 0x55,
+        ];
 
-        let place_block_fn = unsafe {
-            transmute::<*const u8, PlaceBlockFnType>(
-                exe_module_memory.as_ptr().add(place_block_fn_offset),
-            )
-        };
+        let place_block_fn = find_fn(exe_module_memory, pattern, 0)?;
 
         Ok(Self(place_block_fn))
     }
@@ -267,22 +253,14 @@ pub struct RemoveBlockFn(RemoveBlockFnType);
 
 impl RemoveBlockFn {
     pub fn find(exe_module_memory: &[u8]) -> Result<Self, Box<dyn Error>> {
-        let remove_block_fn_offset = memmem::find(
-            exe_module_memory,
-            &[
-                0x48, 0x89, 0x5c, 0x24, 0x08, 0x48, 0x89, 0x6c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24,
-                0x18, 0x57, 0x48, 0x83, 0xec, 0x40, 0x83, 0x7c, 0x24, 0x70, 0x00,
-            ],
-        )
-        .ok_or("failed to find RemoveBlock function pattern")?;
+        let pattern = &[
+            0x48, 0x89, 0x5c, 0x24, 0x08, 0x48, 0x89, 0x6c, 0x24, 0x10, 0x48, 0x89, 0x74, 0x24,
+            0x18, 0x57, 0x48, 0x83, 0xec, 0x40, 0x83, 0x7c, 0x24, 0x70, 0x00,
+        ];
 
-        let remove_block_fn = unsafe {
-            transmute::<*const u8, RemoveBlockFnType>(
-                exe_module_memory.as_ptr().add(remove_block_fn_offset),
-            )
-        };
+        let place_block_fn = find_fn(exe_module_memory, pattern, 0)?;
 
-        Ok(Self(remove_block_fn))
+        Ok(Self(place_block_fn))
     }
 
     pub unsafe fn call(&self, map_editor: &mut MapEditor, block: &mut Block) -> u32 {
@@ -301,22 +279,14 @@ pub struct PlaceItemFn(PlaceItemFnType);
 
 impl PlaceItemFn {
     pub fn find(exe_module_memory: &[u8]) -> Result<Self, Box<dyn Error>> {
-        let place_item_fn_offset = memmem::find(
-            exe_module_memory,
-            &[
-                0x48, 0x89, 0x5c, 0x24, 0x10, 0x48, 0x89, 0x6c, 0x24, 0x18, 0x48, 0x89, 0x74, 0x24,
-                0x20, 0x57, 0x48, 0x83, 0xec, 0x40, 0x49, 0x8b, 0xf9,
-            ],
-        )
-        .ok_or("failed to find PlaceItem function pattern")?;
+        let pattern = &[
+            0x48, 0x89, 0x5c, 0x24, 0x10, 0x48, 0x89, 0x6c, 0x24, 0x18, 0x48, 0x89, 0x74, 0x24,
+            0x20, 0x57, 0x48, 0x83, 0xec, 0x40, 0x49, 0x8b, 0xf9,
+        ];
 
-        let place_item_fn = unsafe {
-            transmute::<*const u8, PlaceItemFnType>(
-                exe_module_memory.as_ptr().add(place_item_fn_offset),
-            )
-        };
+        let place_block_fn = find_fn(exe_module_memory, pattern, 0)?;
 
-        Ok(Self(place_item_fn))
+        Ok(Self(place_block_fn))
     }
 
     #[allow(clippy::too_many_arguments)]
@@ -367,20 +337,12 @@ pub struct RemoveItemFn(RemoveItemFnType);
 
 impl RemoveItemFn {
     pub fn find(exe_module_memory: &[u8]) -> Result<Self, Box<dyn Error>> {
-        let remove_item_fn_offset = memmem::find(
-            exe_module_memory,
-            &[
-                0x48, 0x89, 0x5c, 0x24, 0x08, 0x57, 0x48, 0x83, 0xec, 0x30, 0x48, 0x8b, 0xfa, 0x48,
-                0x8b, 0xd9, 0x48, 0x85, 0xd2, 0x0f, 0x84, 0xe6, 0x00, 0x00, 0x00,
-            ],
-        )
-        .ok_or("failed to find RemoveItem function pattern")?;
+        let pattern = &[
+            0x48, 0x89, 0x5c, 0x24, 0x08, 0x57, 0x48, 0x83, 0xec, 0x30, 0x48, 0x8b, 0xfa, 0x48,
+            0x8b, 0xd9, 0x48, 0x85, 0xd2, 0x0f, 0x84, 0xe6, 0x00, 0x00, 0x00,
+        ];
 
-        let remove_item_fn = unsafe {
-            transmute::<*const u8, RemoveItemFnType>(
-                exe_module_memory.as_ptr().add(remove_item_fn_offset),
-            )
-        };
+        let remove_item_fn = find_fn(exe_module_memory, pattern, 0)?;
 
         Ok(Self(remove_item_fn))
     }
